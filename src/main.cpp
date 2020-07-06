8bb03c62163f606f580e114fa3b9cc2abf6925d9
mykalrudie <mykalrudie@gmail.com>
2020-07-06 16:47:42 -0400

    Uploading Mykal's code

--- src/main.cpp
+++ src/main.cpp
@@ -1,229 +1,113 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
+#include <Servo.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 	-1 // This display does not have a reset pin accessible
 
+#define tapel PB0
+#define taper PB1
+#define motorlf PA_2
+#define motorlb PA_3
+#define motorrf PA_0
+#define motorrb PA_1
+#define threshold 875
+#define maxspeed 512
+#define switch PB12
 
-#define RIGHT_TAPE_SENSOR PA5 //ANALOG
-#define LEFT_TAPE_SENSOR PA4 //ANALOG
+bool tapeonl;
+bool tapeonr;
 
-#define TAPE_SENSOR_POT PA0 //ANALOG
+int speedl;
+int speedr;
 
-#define CORRECTING_SPEED_POT PB0 //ANALOG
-#define DISPLAY_BUTTON PA3 //DIGITAL
+bool speedsl[8];
+bool speedsr[8];
 
-#define RIGHT_MOTOR_FORWARD PB_9 //PWM
-#define RIGHT_MOTOR_REVERSE PA_1 //PWM
+bool lastonl;
 
-#define LEFT_MOTOR_FORWARD PB_8 //PWM
-#define LEFT_MOTOR_REVERSE PA_2 //PWM
-
-#define RIGHT 0
-#define LEFT 1
-
-#define PWM_FREQUENCY 1000
-
-#define STOP 0
-#define FULL_SPEED 512
-#define REVERSE_SPEED 512*0.35
-
-#define FORWARD 1
-#define REVERSE 0
+int halfspeed = maxspeed/2;
+int thirdspeed = 0;
 
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-void readCorrectionSpeed();
-void correctDirection();
-void printMessage(int, bool);
-void printMessage(String, bool);
-void refreshDisplay();
-void readTapeSensors();
-void turn(int);
-void turnWithReverse(int);
-void driveStraight(int);
-void driveMotor(int, int, int);
-
-bool rightReflectance;
-bool leftReflectance;
-unsigned int tapeSensorThreahold;
-
-int lastSideOnTape = RIGHT;
-
-unsigned int correctingSpeed;
-
+void runmotors(int speedl, int speedr);
+void showvalues();
 
 void setup() {
-  // initialize LED digital pin as an output and set on
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
-  pinMode(RIGHT_MOTOR_REVERSE, OUTPUT);
-
-  pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
-  pinMode(LEFT_MOTOR_REVERSE, OUTPUT);
-
-  pinMode(DISPLAY_BUTTON, INPUT_PULLUP);
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
   display.display();
+  delay(2000);
 
-  display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
+
+  display.clearDisplay();
   display.setCursor(0,0);
-  }
+  display.println(analogRead(tapel));
+  display.println(analogRead(taper));
+  display.display();
+  delay(2000);
+   display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(analogRead(tapel));
+  display.println(analogRead(taper));
+  display.display();
+  delay(2000);
+  pinMode(switch, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(switch), showvalues, FALLING);
+
+
+}
 
 void loop() {
-  readCorrectionSpeed();
-  readTapeSensors();
-  if (digitalRead(DISPLAY_BUTTON))
-  {
-    correctDirection();
+  if (analogRead(tapel) > threshold) {
+    tapeonl = true;
+  } else {
+    tapeonl = false;
   }
-  else{
-    driveStraight(STOP);
-    refreshDisplay();
-    printMessage(tapeSensorThreahold, true);
-    printMessage(analogRead(RIGHT_TAPE_SENSOR), false);
-    printMessage("   ", false);
-    printMessage(rightReflectance, true);
-    printMessage(analogRead(LEFT_TAPE_SENSOR), false);
-    printMessage("   ", false);
-    printMessage(leftReflectance, true);
-    printMessage(analogRead(CORRECTING_SPEED_POT), false);
-    printMessage("   ", false);
-    printMessage(correctingSpeed *100 / 512, true);
-    display.display();
+  if (analogRead(taper) > threshold) {
+    tapeonr = true;
+  } else {
+    tapeonr = false;
+  }
+
+  if (tapeonl && tapeonr) {
+    runmotors(maxspeed, maxspeed);
+  }
+
+  if (tapeonl && !tapeonr) {
+    lastonl = true;
+    runmotors(maxspeed, halfspeed);
+  }
+
+  if (tapeonr && !tapeonl) {
+    lastonl = false;
+    runmotors(halfspeed, maxspeed);
+  }
+
+  if (!tapeonr && !tapeonl) {
+    if (lastonl) {
+      runmotors(maxspeed, thirdspeed);
+    } else {
+      runmotors(thirdspeed, maxspeed);
+    }
   }
 }
 
-void readCorrectionSpeed(){
-  correctingSpeed = map(analogRead(CORRECTING_SPEED_POT), 0, 1023, 0, FULL_SPEED);
+void runmotors(int speedl, int speedr) {
+  pwm_start(motorrf, 1024, speedr, RESOLUTION_10B_COMPARE_FORMAT);
+  pwm_start(motorlf, 1024, speedl, RESOLUTION_10B_COMPARE_FORMAT);
+
+
 }
 
-void correctDirection(){
-  if(rightReflectance && leftReflectance){
-    driveStraight(correctingSpeed);
-    //printMessage("Straight", true);
-  }
-  else if (rightReflectance){
-    turn(RIGHT);
-    //turnWithReverse(RIGHT);
-    //printMessage("Turn Right", true);
-  }
-  else if (leftReflectance){
-    turn(LEFT);
-    //turnWithReverse(LEFT);
-    //printMessage("Turn Left", true);
-  }
-  else{
-    turnWithReverse(lastSideOnTape);
-    //turn(lastSideOnTape);
-    // printMessage("OFF");
-    // refreshDisplay();
-    //printMessage(leftReflectance, false);
-    //printMessage(rightReflectance, false);
-    //printMessage("W/ Reverse", false);
-    //printMessage(lastSideOnTape, true);
-    
-  }
-  //display.display();
-}
-
-void printMessage(int message, bool newline){
-  printMessage(String(message), newline);
-}
-
-void printMessage(String message, bool newline){
-   if (newline){
-    display.println(message);
-  }
-  else{
-    display.print(message);
-  }
-}
-
-void refreshDisplay(){
+void showvalues() {
   display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
-}
-
-void readTapeSensors(){
-  //refreshDisplay();
-  tapeSensorThreahold = analogRead(TAPE_SENSOR_POT);
-
-  rightReflectance = analogRead(RIGHT_TAPE_SENSOR) > tapeSensorThreahold;
-  leftReflectance = analogRead(LEFT_TAPE_SENSOR) > tapeSensorThreahold;
-
-  if (rightReflectance && !leftReflectance){
-    lastSideOnTape = RIGHT;
-    
-    //printMessage("Right Last", true);
-  }
-  else if (leftReflectance && !rightReflectance){
-    lastSideOnTape = LEFT;
-    //printMessage("Left Last", true);
-  }
-}
-
-void turn(int side){
-  if (side == RIGHT) {
-    driveMotor(LEFT, correctingSpeed, FORWARD);
-    driveMotor(RIGHT, STOP, FORWARD);
-  }
-  else if (side == LEFT) {
-    driveMotor(RIGHT, correctingSpeed, FORWARD);
-    driveMotor(LEFT, STOP, FORWARD);
-  }
-}
-
-void turnWithReverse(int side){
-  if (side == RIGHT) {
-    driveMotor(LEFT, correctingSpeed, FORWARD);
-    driveMotor(RIGHT, REVERSE_SPEED, REVERSE);
-  }
-  else if (side == LEFT) {
-    driveMotor(RIGHT, correctingSpeed, FORWARD);
-    driveMotor(LEFT, REVERSE_SPEED, REVERSE);
-  }
-}
-
-void driveStraight(int speed){
-  driveMotor(RIGHT, speed, FORWARD);
-  driveMotor(LEFT, speed, FORWARD);
-}
-
-void driveMotor(int motorSide, int speed, int direction){
-  if (motorSide == RIGHT) {
-    if (direction == FORWARD) {
-      pwm_start(RIGHT_MOTOR_REVERSE, PWM_FREQUENCY, 0, RESOLUTION_9B_COMPARE_FORMAT);
-      pwm_start(RIGHT_MOTOR_FORWARD, PWM_FREQUENCY, speed, RESOLUTION_9B_COMPARE_FORMAT);
-    }
-    else if (direction == REVERSE){
-      // refreshDisplay();
-      // printMessage("Right Reverse", true);
-      // display.display();
-      pwm_start(RIGHT_MOTOR_FORWARD, PWM_FREQUENCY, 0, RESOLUTION_9B_COMPARE_FORMAT);
-      pwm_start(RIGHT_MOTOR_REVERSE, PWM_FREQUENCY, speed, RESOLUTION_9B_COMPARE_FORMAT);
-    }
-  }
-  else if (motorSide == LEFT) {
-    if (direction == FORWARD) {
-      pwm_start(LEFT_MOTOR_REVERSE, PWM_FREQUENCY, 0, RESOLUTION_9B_COMPARE_FORMAT);
-      pwm_start(LEFT_MOTOR_FORWARD, PWM_FREQUENCY, speed, RESOLUTION_9B_COMPARE_FORMAT);
-    }
-    else if (direction == REVERSE){
-      // refreshDisplay();
-      // printMessage("Left Reverse", true);
-      // display.display();
-      pwm_start(LEFT_MOTOR_FORWARD, PWM_FREQUENCY, 0, RESOLUTION_9B_COMPARE_FORMAT);
-      pwm_start(LEFT_MOTOR_REVERSE, PWM_FREQUENCY, speed, RESOLUTION_9B_COMPARE_FORMAT);
-    }
-  }
+  display.println(analogRead(tapel));
+  display.println(analogRead(taper));
+  display.display();
 }
