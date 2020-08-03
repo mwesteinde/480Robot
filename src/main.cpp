55d87f2267529a765d67e905ed0717a455d3d8d1
mykalrudie <mykalrudie@gmail.com>
2020-08-02 21:38:27 -0400

    Ball release and launch for beer pong working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-//#include <Servo.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -19,12 +18,20 @@
 #define displayswitch PB4
 
 #define servopin PA_8
+#define ballservo PA_1
 
 #define startpin PB5
 
-#define maxspeed 900 //max speed of motor
+#define correctingspeed PA0
+#define turnspeed PA1
+
+#define maxspeed 512 //max speed of motor
+#define max2 400
 #define threshold 400 //tape on/off threshold
-#define rotorspeed 512
+#define rotorspeed 350
+#define pwmfrequency 512
+#define partyspeed 1023
+#define STOP 0
 
 bool tapeonl;
 bool tapeonr;
@@ -33,27 +40,33 @@ bool lastr;
 
 int speedl;
 int speedr;
+int i = 100;
 
 //bool speedsl[8];
 //bool speedsr[8];
 
 bool lastonl;
 
-int halfspeed = maxspeed/2;
-int thirdspeed = maxspeed/3;
+int halfspeed = maxspeed - 150;
+int thirdspeed = maxspeed/2;
 int fourthspeed = 0;
-int fifthspeed = -maxspeed;
+int fifthspeed = -maxspeed - 150;
 //int i = 300;
 
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void runmotors(int speedl, int speedr);
 void showvalues();
+void readpots();
 void hitchcontrol(int angle);
+void partymode();
+void ballrelease();
+void shoot();
+
 
 void setup() {
    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  // Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
+   //Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
   display.display();
   delay(250);
   display.setTextSize(2);
@@ -61,15 +74,38 @@ void setup() {
 
   pinMode(displayswitch, INPUT_PULLUP);
   pinMode(startpin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(displayswitch), showvalues, FALLING);
-  hitchcontrol(180);
-  while(digitalRead(startpin)) {
+  pinMode(correctingspeed, INPUT_PULLDOWN);
+  pinMode(turnspeed, INPUT_ANALOG);
+
+  if(digitalRead(correctingspeed)) {
+    display.clearDisplay();
+    display.println("PartyMode Ready");
+    display.display();
+    while(digitalRead(displayswitch)) {
+    }
+    pinMode(correctingspeed, INPUT_ANALOG);
+    partymode();
   }
-  delay(1000);
+  hitchcontrol(180);
+  if(digitalRead(startpin)) {
+    display.clearDisplay();
+    display.println("Ready2Rumble");
+    display.display();
+    while (digitalRead(displayswitch)) {
+      hitchcontrol(180);
+      delay(20);
+    } 
+    display.clearDisplay();
+    display.println("Run Forrest!");
+    display.display();
+  }
+  attachInterrupt(digitalPinToInterrupt(displayswitch), showvalues, FALLING);
+  //delay(1000);
   hitchcontrol(130);
   delay(500);
-  pwm_start(rotor, 1024, rotorspeed, RESOLUTION_10B_COMPARE_FORMAT);
+  pwm_start(rotor, 1024, rotorspeed, RESOLUTION_9B_COMPARE_FORMAT);
 }
+
 
 void loop() {
   if (analogRead(tapel) > threshold) {
@@ -99,9 +135,9 @@ void loop() {
   if (tapeonl && !tapeonr) {
     lastonl = true;
     if (lastr) {
-      runmotors(halfspeed, maxspeed);
-    } else {
       runmotors(thirdspeed, maxspeed);
+    } else {
+      runmotors(halfspeed, maxspeed);
     }
     
   }
@@ -109,9 +145,9 @@ void loop() {
   if (tapeonr && !tapeonl) {
     lastonl = false;
     if (lastl) {
-      runmotors(maxspeed, halfspeed);
-    } else {
       runmotors(maxspeed, thirdspeed);
+    } else {
+      runmotors(maxspeed, halfspeed);
     }
   }
 
@@ -130,35 +166,24 @@ void loop() {
       }
     }
   }
-  if (digitalRead(startpin)) {
-    delay(10);
-  } else {
-    delay(10);
-  }
-
-  
-
-
+  delay(10);
 }
 
 void runmotors(int speedl, int speedr) {
   if (speedl >= 0) {
-    pwm_start(motorlb, 1024, 0, RESOLUTION_10B_COMPARE_FORMAT);
-    pwm_start(motorlf, 1024, speedl, RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(motorlb, pwmfrequency, 0, RESOLUTION_9B_COMPARE_FORMAT);
+    pwm_start(motorlf, pwmfrequency, speedl, RESOLUTION_9B_COMPARE_FORMAT);
   } else {
-    pwm_start(motorlf, 1024, 0, RESOLUTION_10B_COMPARE_FORMAT);
-    pwm_start(motorlb, 1024, -speedl, RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(motorlf, pwmfrequency, 0, RESOLUTION_9B_COMPARE_FORMAT);
+    pwm_start(motorlb, pwmfrequency, -speedl, RESOLUTION_9B_COMPARE_FORMAT);
   }
   if (speedr >= 0) {
-    pwm_start(motorrb, 1024, 0, RESOLUTION_10B_COMPARE_FORMAT);
-    pwm_start(motorrf, 1024, speedr, RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(motorrb, pwmfrequency, 0, RESOLUTION_9B_COMPARE_FORMAT);
+    pwm_start(motorrf, pwmfrequency, speedr, RESOLUTION_9B_COMPARE_FORMAT);
   } else {
-    pwm_start(motorrf, 1024, 0, RESOLUTION_10B_COMPARE_FORMAT);
-    pwm_start(motorrb, 1024, -speedr, RESOLUTION_10B_COMPARE_FORMAT);
+    pwm_start(motorrf, pwmfrequency, 0, RESOLUTION_9B_COMPARE_FORMAT);
+    pwm_start(motorrb, pwmfrequency, -speedr, RESOLUTION_9B_COMPARE_FORMAT);
   }
-  
-  
-
 }
 
 void hitchcontrol(int angle) {
@@ -166,10 +191,46 @@ void hitchcontrol(int angle) {
   pwm_start(servopin, 50, pwmsignal, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
 }
 
+void readpots(){
+  thirdspeed = map(analogRead(correctingspeed), 0, 1024, 0, 511);
+  fifthspeed = -map(analogRead(turnspeed), 0, 1024, 0, 511);
+}
+
 void showvalues() {
   display.clearDisplay();
   display.setCursor(0,0);
+  display.print("left: ");
   display.println(analogRead(tapel));
+  display.print("right: ");
   display.println(analogRead(taper));
+
   display.display();
 }
+
+void partymode() {
+  while(true) {
+    ballrelease();
+    delay(500);
+    pwm_start(rotor, 512, 512, RESOLUTION_9B_COMPARE_FORMAT);
+    delay(300);
+    pwm_start(rotor, 512, STOP, RESOLUTION_9B_COMPARE_FORMAT);
+    delay(500);
+  }
+}
+
+void shoot() {
+
+}
+
+void ballrelease() {
+  //int pwmsignal = map(660, 0, 1024, 500, 2500);
+  pwm_start(ballservo, 50, 1600, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+  /*display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(analogRead(correctingspeed));
+  display.display();*/
+  delay(98);
+  pwm_start(ballservo, 50, 2500, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+  delay(3000);
+}
+
